7a06cc34c80a3f4ae616caa981ac403dcc177bc4
Gubitelson <119538200+Gubitelson@users.noreply.github.com>
2022-12-19 21:27:02 +0400

    first commit

---
+++ homework C# 1/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Максимальное = {num1}, Минимальное = {num2}");
+}
+else
+{
+    Console.WriteLine($"Максимальное = {num2}, Минимальное = {num1}");
+}
---
+++ homework C# 2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.Clear();
+
+Console.WriteLine("введи первое число:");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введи второе число:");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введи третье число:");
+int Num3 = Convert.ToInt32(Console.ReadLine());
+
+if (Num1 > Num2 && Num1 > Num3)
+{
+    
+    Console.WriteLine($"Максимальное значение = {Num1} ");
+
+}
+else if (Num3 > Num2)
+{
+    Console.WriteLine($"Максимальное значение = {Num3} ");
+}
+
+else
+{
+    Console.WriteLine($"Максимальное значение = {Num2} ");
+}
